752921c8c330c85d0d2a8f20c5f8255dfbfdb45d
NiamYoseph <liamcorbett97@gmail.com>
2018-05-27 00:01:16 -0400

    Added pure.css to site
    Also changed double quotes to single quotes :'(

--- puddle.py
+++ puddle.py
@@ -3,20 +3,20 @@ app = Flask(__name__)
 
 puddles = [
     {
-        "author": "Liam Joseph Corbett",
-        "title": "Example puddle",
-        "body": "Hi, this is a puddle!",
-        "timestamp": "May 26, 2018"
+        'author': 'Liam Joseph Corbett',
+        'title': 'Example puddle',
+        'body': 'Hi, this is a puddle!',
+        'timestamp': 'May 26, 2018'
     },
     {
-        "author": "Liam Joseph Corbett",
-        "title": "Example puddle 2",
-        "body": "Hi, this is another puddle!",
-        "timestamp": "May 26, 2018"
+        'author': 'Liam Joseph Corbett',
+        'title': 'Example puddle 2',
+        'body': 'Hi, this is another puddle!',
+        'timestamp': 'May 26, 2018'
     }
 ]
 
-@app.route("/")
-@app.route("/home")
+@app.route('/')
+@app.route('/home')
 def home():
-    return render_template("home.html", puddles=puddles)
+    return render_template('home.html', puddles=puddles)
