85ecc07a8ea10b1d4a5f731728eac87fba209f76
NiamYoseph <liamcorbett97@gmail.com>
2018-05-26 23:25:04 -0400

    Added example post JINJA; created Flask layout

--- puddle.py
+++ puddle.py
@@ -1,7 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+puddles = [
+    {
+        "author": "Liam Joseph Corbett",
+        "title": "Example puddle",
+        "body": "Hi, this is a puddle!",
+        "timestamp": "May 26, 2018"
+    },
+    {
+        "author": "Liam Joseph Corbett",
+        "title": "Example puddle 2",
+        "body": "Hi, this is another puddle!",
+        "timestamp": "May 26, 2018"
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", puddles=puddles)
